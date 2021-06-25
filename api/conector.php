2b9c8f9ca70472b00ca7191166a9a1d1bc8b96e6
Igor Augusto Dos Santos <igoraugusto1029@gmail.com>
2021-06-24 22:36:53 -0300

    implementa método para conectar ao banco de dados

--- api/conector.php
+++ api/conector.php
@@ -1,18 +1,33 @@
 <?php
     include_once __DIR__ . '/vendor/autoload.php';
 
-    class conector{
+    class Conector{
         protected $link;
+        private $getEvento = "SELECT * FROM saphira_evento WHERE ID_evento=?";
 
         function __construct()
         {
             $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
             $dotenv->load(); 
-            $this->conectar();
+        }
+            //TODO: separar o metodo conectar em: conectar, getEvento
+        public function conectar($evento){
+            $this->link = mysqli_connect($_ENV['local'], $_ENV['nome'], $_ENV['senha'], $_ENV['db']);
+            if(!$this->link){
+                throw new Exception("Não foi possível conectar ao banco de dados");
+            }
+            
+            $prepara = $this->link->prepare($this->getEvento);
+            $prepara->bind_param('i', $evento);
+            $prepara->execute();
+            $resultado = $prepara->get_result()->fetch_assoc();
+            
+            return $resultado;
         }
 
-        private function conectar(){
-            $this->link = mysqli_connect($_ENV['local'], $_ENV['nome'], $_ENV['senha'], $_ENV['db']);
+        public function login($id, $hash){
+
         }
+
     }
 ?>
