c5d4176d5b8045d9ffd62184deb5e079a21ada7c
IgorSantoss <igoraugusto1029@gmail.com>
2021-06-25 17:57:32 -0300

    Implementa login banco de dados

--- api/api.php
+++ api/api.php
@@ -5,16 +5,19 @@
 <?php    
 
     if(isset($_GET['value'])){
-        $conec = new Conector();
+        $conec;
 
         try{
-            $evento = $conec->conectar($_GET['value']);
+            $conec = new Conector();
+            $resp = $conec->login("SSIOnline", 6, $_GET['value']);
 
             http_response_code(200);
-            echo json_encode($evento, JSON_PRETTY_PRINT);
+            echo json_encode($resp, JSON_PRETTY_PRINT);
         }catch(Exception $e){
             http_response_code(500);
         }
+
+
     }else{
         http_response_code(404);
     }
--- api/conector.php
+++ api/conector.php
@@ -2,31 +2,42 @@
     include_once __DIR__ . '/vendor/autoload.php';
 
     class Conector{
-        protected $link;
-        private $getEvento = "SELECT * FROM saphira_evento WHERE ID_evento=?";
+        private $link;
+        private $getEventoQuery = "SELECT * FROM saphira_evento WHERE ID_evento=?";
+        private $loginQuery = "SELECT Senha FROM saphira_usuario WHERE Login=? AND ID_evento=?";
 
         function __construct()
         {
             $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-            $dotenv->load(); 
+            $dotenv->load();
+            $this->conectar();
         }
             //TODO: separar o metodo conectar em: conectar, getEvento
-        public function conectar($evento){
-            $this->link = mysqli_connect($_ENV['local'], $_ENV['nome'], $_ENV['senha'], $_ENV['db']);
-            if(!$this->link){
-                throw new Exception("Não foi possível conectar ao banco de dados");
+        public function conectar(){
+            $this->link = new mysqli($_ENV['local'], $_ENV['nome'], $_ENV['senha'], $_ENV['db']); //mysqli_connect($_ENV['local'], $_ENV['nome'], $_ENV['senha'], $_ENV['db']);
+            if($this->link->connect_errno){
+                throw new RuntimeException("Não foi possível conectar ao banco de dados");
             }
-            
-            $prepara = $this->link->prepare($this->getEvento);
+                  
+            return true;
+        }
+
+        public function getEvento($evento){
+            $prepara = $this->link->prepare($this->getEventoQuery);
             $prepara->bind_param('i', $evento);
             $prepara->execute();
             $resultado = $prepara->get_result()->fetch_assoc();
-            
+
             return $resultado;
         }
 
-        public function login($id, $hash){
+        public function login($login, $IdEvento, $senha){
+            $prepara = $this->link->prepare($this->loginQuery);
+            $prepara->bind_param('si', $login, $IdEvento);
+            $prepara->execute();
+            $resultado = $prepara->get_result()->fetch_assoc();
 
+            return password_verify($senha, $resultado['Senha']);            
         }
 
     }
