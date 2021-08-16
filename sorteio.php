a8eda31b1c5ee0de2ea715daf8ba944e62184343
Andre Casaes <andre.casaes@usp.br>
2021-08-16 17:32:19 -0300

    removendo hora

--- sorteio.php
+++ sorteio.php
@@ -31,11 +31,10 @@ function pause() {
 }
 
 function reset() {
-  hour = 0;
   minute = 0;
   second = 0;
   millisecond = 0;
-  document.getElementById('hour').innerText = '00';
+
   document.getElementById('minute').innerText = '00';
   document.getElementById('second').innerText = '00';
   document.getElementById('millisecond').innerText = '000';
@@ -50,11 +49,6 @@ function timer() {
     second = 0;
     minute++;
   }
-  if (minute == 60) {
-    minute = 0;
-    hour++;
-  }
-  document.getElementById('hour').innerText = returnData(hour);
   document.getElementById('minute').innerText = returnData(minute);
   document.getElementById('second').innerText = returnData(second);
   document.getElementById('millisecond').innerText = returnData(millisecond);
